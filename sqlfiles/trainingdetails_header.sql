39ef2c0e43c8274f4092ec96d97733c20bb36e40
elekdra <swalitm@gmail.com>
2021-11-28 15:18:27 +0530

    modified

--- sqlfiles/trainingdetails_header.sql
+++ sqlfiles/trainingdetails_header.sql
@@ -1,7 +1,6 @@
-DROP TABLE `document_management`.`trainingdetails_header`;
-USE `DOCUMENT_MANAGEMENT`;
-Create table TrainingDetails_data (`DATA_ID` INT NOT NULL auto_increment,`Training_index` INT NOT NULL,`Filepath` varchar(50),`minimum_version` varchar(10),primary key(`DATA_ID`));
-ALTER TABLE TrainingDetails_data AUTO_INCREMENT=1;
-INSERT INTO TrainingDetails_data(`Training_index`,`Filepath`,`minimum_version`) Values(1,'ertuifghj','1.0'),(1,'ertuifghj','1.0'),(1,'ertuifghj','1.0'),(1,'ertuifghj','1.0'),(1,'ertuifghj','1.0'),(1,'ertuifghj','1.0'),(1,'ertuifghj','1.0'),(1,'ertuifghj','1.0'),(1,'ertuifghj','1.0'),(2,'ertuifghj','1.0'),(2,'ertuifghj','1.0'),(3,'ertuifghj','1.0'),(3,'ertuifghj','1.0'),(4,'ertuifghj','1.0'),(4,'ertuifghj','1.0'),(5,'ertuifghj','1.0'),(5,'ertuifghj','1.0'),(6,'ertuifghj','1.0'),(6,'ertuifghj','1.0'),(6,'ertuifghj','1.0'),(7,'ertuifghj','1.0'),(7,'ertuifghj','1.0'),(7,'ertuifghj','1.0');
 
+USE `DOCUMENT_MANAGEMENT`;
+Create table TrainingDetails_header (`Training_index` INT NOT NULL auto_increment,`Company_ID` INT NOT NULL,`Version` varchar(50),`Training_ID` INT NOT NULL,primary key(`Training_index`));
+
+INSERT INTO TrainingDetails_header(`Company_ID`,`Version`,`Training_ID`) Values(101,'1.0',1001),(102,'1.0',1001),(103,'1.0',1001),(104,'1.0',1001),(105,'1.0',1001),(101,'1.0',1002),(102,'1.0',1002),(103,'1.0',1002),(104,'1.0',1002),(105,'1.0',1002),(101,'1.0',1003),(102,'1.0',1003),(103,'1.0',1003),(104,'1.0',1003),(105,'1.0',1003),(101,'1.0',1004);
 
